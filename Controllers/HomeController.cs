9d29e975e117319bfcb7568777ba64b32c53756e
Adın Soyadın <mmusayeva@std.beu.edu.az>
2026-04-26 21:31:55 +0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication10.Models;
+using System.Data.OleDb;
+using System.Collections.Generic;
+
+namespace WebApplication10.Controllers
+{
+    public class HomeController : Controller
+    {
+        string connectionString =
+@"Provider=Microsoft.ACE.OLEDB.12.0;
+Data Source=C:\Users\hp\OneDrive\Documents\Student.accdb";
+
+        // 🔥 BURASI VACİBDİR
+        static List<StudentCourse> list = new List<StudentCourse>();
+
+        public IActionResult Index()
+        {
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Index(string studentName, string courseName)
+        {
+            StudentCourse model = new StudentCourse
+            {
+                StudentName = studentName,
+                CourseName = courseName,
+                CurrentCount = 1
+            };
+
+            if (courseName == "Csharp")
+                model.Limit = 20;
+            else if (courseName == "Riyaziyyat")
+                model.Limit = 2;
+            else
+                model.Limit = 15;
+
+            // 🔥 list-ə əlavə edirik
+            list.Add(model);
+
+            return View(list);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/StudentCourse.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication10.Models
+{
+    public class StudentCourse
+    {
+        public string StudentName { get; set; }
+
+        public string CourseName { get; set; }
+
+        public int CurrentCount { get; set; }
+
+        public int Limit { get; set; }
+    }
+}
